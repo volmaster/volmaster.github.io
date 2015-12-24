7fda92ad29bf1242aa0fa079640a2c8df56a5e5f
volmaster <volmasters@gmail.com>
2015-12-25 00:45:44 +0200

    -

--- works/Logo/js/main.js
+++ works/Logo/js/main.js
@@ -1,11 +1,14 @@
 //Vivus
 $(function () {
 
-    new Vivus('#svg2', {duration: 350},
-        function () {
-            if (window.console) {
-                console.log('Animation finished');
-            }
-        });
+    //new Vivus('#svg2', {duration: 350},
+    //    function () {
+    //        if (window.console) {
+    //            console.log('Animation finished');
+    //        }
+    //    });
+    new Vivus('#svg2', {}, function (obj) {
+        obj.el.classList.add('finished');
+    });
 
 });

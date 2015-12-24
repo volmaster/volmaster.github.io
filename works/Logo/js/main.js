edcb28423cbc4dee2da8df88dc328ca0f4a2d02c
volmaster <volmasters@gmail.com>
2015-12-25 00:41:58 +0200

    -

--- works/Logo/js/main.js
+++ works/Logo/js/main.js
@@ -1,11 +1,11 @@
-
 //Vivus
+$(function () {
 
-new Vivus('#svg2', { duration: 350},
-    function () {
-        if (window.console) {
-            console.log('Animation finished');
-        }
-    });
+    new Vivus('#svg2', {duration: 350},
+        function () {
+            if (window.console) {
+                console.log('Animation finished');
+            }
+        });
 
-
+});

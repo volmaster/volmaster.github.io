ca1af9abcf657864bda35bf62b271134760752c1
volmaster <volmasters@gmail.com>
2015-12-25 00:02:31 +0200

    .

---
+++ works/Logo/js/main.js
@@ -0,0 +1,11 @@
+
+//Vivus
+
+new Vivus('svg-logo', {type: 'oneByOne', duration: 350, start: 'autostart'},
+    function () {
+        if (window.console) {
+            console.log('Animation finished');
+        }
+    });
+
+
